b7ecc9ccf38302e91a03be9ec4fa2f6fbc30f0ce
Yegor Lukash <yegor.lukash@gmail.com>
2011-09-26 16:47:06 +0700

    Изменил сайдбар

--- htdocs/user/extras/sidebar-post.tmpl.php
+++ htdocs/user/extras/sidebar-post.tmpl.php
@@ -2,7 +2,6 @@
 <h2>Хорошие блоги</h2>
 
 <ul class="links-list">
-<li><a href="http://ilyabirman.ru/">Илья Бирман</a></li>
 <li><a href="http://bolknote.ru/">Евгений Степанищев</a></li>
 <li><a href="http://ksoftware.livejournal.com/">Коля Товеровский</a></li>
 <li><a href="http://tema.livejournal.com/">Тёма Лебедев</a></li>
@@ -17,11 +16,11 @@
 <h2>Любимые сайты</h2>
 
 <ul class="links-list">
-<li><a href="http://therules.ru/">Правила русского языка</a></li>
+<li><a href="http://habrahabr.ru/">Хабр</a></li>
+<li><a href="http://startuppoint.ru/">СтартапПоинт</a></li>
 <li><a href="http://design.artgorbunov.ru/">Дизайн-собака</a></li>
-<li><a href="http://aiznashegookna.ru/">Аизнашегоокна</a></li>
-<li><a href="http://biathlontime.com/">Биатлонтайм</a></li>
+<li><a href="http://google.com/">Гугл, че</a></li>
 <li><a href="http://stackoverflow.com/">Стековерфлоу</a></li>
-<li><a href="http://shorpy.com/">Шорпи</a></li>
+<li><a href="http://badoo.com/">А здесь хочу работать</a></li>
 </ul>
 </div>
